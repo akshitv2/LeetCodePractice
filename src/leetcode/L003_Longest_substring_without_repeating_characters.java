ace74f56c3cb66b8a16ae0b47d480d4252d41ce4
akshitv2 <ak.bisht1@gmail.com>
2025-09-13 19:06:02 +0530

    13.09.25 Leetcode Problem Solved

---
+++ src/leetcode/L003_Longest_substring_without_repeating_characters.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L003_Longest_substring_without_repeating_characters {
+    public static void main(String[] args) {
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+//        if(s.length()==)
+        Map<Character,Integer> mp = new HashMap<>();
+        int start = 0;
+        int maxLength = 0;
+        for(int i=0;i<s.length();i++){
+            mp.put(s.charAt(i),0);
+        }
+        for(int i=1;i<s.length()+1;i++){
+            start = Math.max(start,mp.get(s.charAt(i-1)));
+            maxLength = Math.max(maxLength, i - start);
+            mp.put(s.charAt(i-1),i);
+        }
+        return maxLength;
+    }
+
+}
