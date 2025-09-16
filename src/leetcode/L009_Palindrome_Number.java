2b896422d44f1b1b57d85607c3ad7d448da0aa44
akshitv2 <ak.bisht1@gmail.com>
2025-09-16 18:42:50 +0530

    16.09.25 Leetcode Problem Solved

---
+++ src/leetcode/L009_Palindrome_Number.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class L009_Palindrome_Number {
+    public static void main(String[] args) {
+    }
+    public static boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int xx = x;
+        int newPow = 0;
+        while(x>0){
+            newPow = newPow * 10 + x % 10;
+            x = x / 10;
+        }
+        return xx == newPow;
+    }
+
+}
